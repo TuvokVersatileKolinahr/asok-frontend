cd6c3425ce85dda75b02532b4016aa0fc5ca9151
Wouter <tuvokki@gmail.com>
2014-03-07 21:03:46 +0100

    The instruments are added. An interface that works also with deletes and stuff Also some cleaning up done.

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,36 @@
 require.config({
   paths: {
-    "jquery": "http://code.jquery.com/jquery-1.10.2.min",
-    // "domReady": "lib/domReady",
-    // "underscore": "lib/underscore",
     "text": "lib/text",
     "Ractive": "lib/Ractive",
+    "Ractiveevents-tap": "lib/Ractive-events-tap",
     "rv": "lib/rv",
     "json": "lib/json"
   }
 });
 
-require([ 'Ractive', 'rv!templates/main', 'rv!templates/navlist', 'json!/hapi/hello' , 'json!/hapi/navlist', 'jquery' ],
-  function ( Ractive, mainTmpl, navListTmpl, hapihello, hapinavlist, $ ) {
+require([ 'Ractive', 'Ractiveevents-tap', 'rv!templates/main', 'rv!templates/navlist', 'json!/hapi/hello' , 'json!/hapi/navlist' ],
+  function ( Ractive, Ractivetap, mainTmpl, navListTmpl, hapihello, hapinavlist ) {
 
   // create the hello world greeting based on the hapi hello backend
-  /*
-  hapihello Object {
-    greeting: "Hello"
-    recipient: "world"
-    }
-  */
   var greet = new Ractive({
     el: 'container',
     template: mainTmpl,
     data: hapihello
   });
-  // attach a listener to the Say! button
+  // attach a listener to the New greeting! button
   listener = greet.on({
     getgreet: function () {
-      $.getJSON( "/hapi/hello").done(function( data ) {
+      xhr = new XMLHttpRequest();
+      xhr.open( 'get', '/hapi/hello' );
+      xhr.onload = function () {
+        var data = JSON.parse( xhr.responseText );
         greet.set({
           greeting: data.greeting,
-          recipient: data.recipient
+          recipient: data.recipient,
+          language: data.language
         });
-      });
+      };
+      xhr.send();
     }
   });
 
